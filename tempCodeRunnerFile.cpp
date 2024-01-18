43d85506dd34bc75309ea50513af301a09e93bd8
Shrey <111275939+shreyshukla008@users.noreply.github.com>
2024-01-18 22:55:33 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+for(auto itr :  t1.player_data)
+                // {
+                //    if(  (itr.second)->status  == -1 )
+                //         cout<<itr.first<<"    "<<t1.p_id[itr.first]<<endl;
+
+                // }
